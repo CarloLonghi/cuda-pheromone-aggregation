9f5fcbc2d8e3a502bd43a4af458ab057601181cd
nema <nemanja.antonic99@gmail.com>
2024-09-03 14:41:08 +0200

    chemotaxis works

--- main.cu
+++ main.cu
@@ -6,25 +6,26 @@
 using json = nlohmann::json;
 
 #define N 128                        // Grid size
-#define WORM_COUNT 1                 // Number of agents
+#define WORM_COUNT 20                 // Number of agents
 #define WIDTH 20.0f          // Width of the 2D space
 #define HEIGHT 20.0f         // Height of the 2D space
 #define BLOCK_SIZE 32        // CUDA block size
 #define N_STEPS 2000         // Number of simulation steps
 #define LOGGING_INTERVAL 10    // Logging interval for saving positions
-#define SPEED 0.022f            // Constant speed at which agents move
+#define SPEED 0.015f            // Constant speed at which agents move
 //#define DX WIDTH/N               // Grid spacing
 #define LAMBDA 0.9f             //persistance of the movement
 #define DRIFT_FACTOR 0.01f       //drift factor
-#define SENSING_RANGE 20        //sensing range of the agents
+#define SENSING_RANGE 1        //sensing range of the agents
 #define MAX_CONCENTRATION 100.0f //maximum concentration of the chemical
-#define DT 0.01f                //time step
-#define GAMMA 0.0001f             //decay rate of the chemical
+#define DT 1.0f                //time step
+#define GAMMA 0.000001f             //decay rate of the chemical
 #define DIFFUSION_CONSTANT 0.001f                  //diffusion rate of the chemical
 #define ATTRACTION_STRENGTH 0.111f
 #define ATTRACTION_SCALE 1.5f
-#define ODOR_THRESHOLD 1.0f
-
+#define ODOR_THRESHOLD 0.1f
+#define DEBUG false
+#define SIGMA 0.001f
 
 __constant__ float DX = WIDTH/N;
 
@@ -112,15 +113,12 @@ __global__ void initGrid(float* grid) {
     }
 }
 
+
+
 // CUDA kernel to update the position of each agent
-__global__ void moveAgents(Agent* agents, curandState* states, float* grid) {
+__global__ void moveAgents(Agent* agents, curandState* states, float* grid, float*potential) {
     int id = threadIdx.x + blockIdx.x * blockDim.x;
     if (id < WORM_COUNT) {
-        //float dx = (curand_uniform(&states[id]) - 0.5f) * 2.0f;  // Move in x direction
-        //float dy = (curand_uniform(&states[id]) - 0.5f) * 2.0f;  // Move in y direction
-        //float dx = (target_x - agents[id].x) * SPEED;
-        //float dy = (target_y - agents[id].y) * SPEED;
-
         //find the highest concentration of the chemical in the sensing range
         float max_concentration = 0.0f;
         int max_concentration_x = 0;
@@ -141,9 +139,7 @@ __global__ void moveAgents(Agent* agents, curandState* states, float* grid) {
                 if (xIndex >= 0 && xIndex < N && yIndex >= 0 && yIndex < N) {
                     concentration = grid[xIndex * N + yIndex];
                 }
-                //if(id==0) {
-                 //   printf("Agent %d sensed concentration %f at (%d, %d)\n", id, concentration, xIndex, yIndex);
-                //}
+
                 if (concentration > max_concentration) {
                     max_concentration = concentration;
                     max_concentration_x = i;
@@ -162,21 +158,27 @@ __global__ void moveAgents(Agent* agents, curandState* states, float* grid) {
         fx /= len;
         fy /= len;
         float new_angle = atan2(fy, fx);
-        //float new_angle = LAMBDA * agents[id].angle + (1.0f - LAMBDA) * (curand_uniform(&states[id]) * 2.0f * M_PI + DRIFT_FACTOR * max_concentration*bias);
-        float sensed_odor = grid[agent_x * N + agent_y];
-        float new_speed = SPEED;
-        if(sensed_odor>0.0f){// || sensed_odor<ODOR_THRESHOLD){
-            new_speed = ATTRACTION_STRENGTH * log10(ATTRACTION_SCALE + sensed_odor);
+        float new_speed_x = SPEED + curand_uniform(&states[id]) * SIGMA;
+        float new_speed_y = SPEED + curand_uniform(&states[id]) * SIGMA;
+        if(max_concentration>ODOR_THRESHOLD){// || sensed_odor<ODOR_THRESHOLD){
+            float potential_x = gradientX(potential, agent_x, agent_y);
+            float potential_y = gradientY(potential, agent_x, agent_y);
+            //printf("Potential x: %f, Potential y: %f\n", potential_x, potential_y);
+            //printf("Sensed odor: %f\n", sensed_odor);
+            new_speed_x =  abs(potential_x) + curand_uniform(&states[id]) * SIGMA;
+            new_speed_y = abs(potential_y) + curand_uniform(&states[id]) * SIGMA;
+            //printf("new speed x: %f, new speed y: %f\n", new_speed_x, new_speed_y);
+
         }
-        //if (new_speed < 0) new_speed = 0;
-        float dx = fx * new_speed;
-        float dy = fy * new_speed;
+
+        float dx = fx * new_speed_x;
+        float dy = fy * new_speed_y;
 
 
         agents[id].x += dx;
         agents[id].y += dy;
         agents[id].angle = new_angle;
-        agents[id].speed = new_speed;
+        //agents[id].speed = new_speed;
         // Apply periodic boundary conditions
         if (agents[id].x < 0) agents[id].x += WIDTH;
         if (agents[id].x >= WIDTH) agents[id].x -= WIDTH;
@@ -194,7 +196,7 @@ __global__ void updateGrid(float* grid) {
         float laplacian_value = laplacian(grid, i, j);
 
         float new_concentration = grid[i * N + j] + DT * (DIFFUSION_CONSTANT * laplacian_value - GAMMA * grid[i * N + j]);
-        if (new_concentration < 0) new_concentration = 0;
+        if (new_concentration < 0) new_concentration = 0.0f;
         if (new_concentration > MAX_CONCENTRATION) new_concentration = MAX_CONCENTRATION;
         //check if the grid is a valid float number
         if (isnan(new_concentration) || isinf(new_concentration)) {
@@ -205,6 +207,15 @@ __global__ void updateGrid(float* grid) {
         }
 
         grid[i * N + j] = new_concentration;
+    }
+}
+
+//CUDA kernel to update the potential matrix
+__global__ void updatePotential(float* potential, float* grid){
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int j = threadIdx.y + blockIdx.y * blockDim.y;
+    if (i < N && j < N) {
+        potential[i * N + j] = ATTRACTION_STRENGTH * log10(ATTRACTION_SCALE + grid[i * N + j]);
     }
 }
 
@@ -267,9 +278,14 @@ int main() {
     float target_y = HEIGHT / 2;
     float* grid;
     float* h_grid = new float[N * N];
+    float* h_potential = new float[N * N];
+    float* potential;
+
+
     cudaMalloc(&d_agents, size);
     cudaMalloc(&d_states, WORM_COUNT * sizeof(curandState));
     cudaMalloc(&grid, N*N*sizeof(float));
+    cudaMalloc(&potential, N*N*sizeof(float));
 
     // Initialize agent positions and random states
     initAgents<<<(WORM_COUNT + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>>(d_agents, d_states, time(NULL));
@@ -289,9 +305,17 @@ int main() {
         printf("CUDA error in initGrid: %s\n", cudaGetErrorString(err));
     }
 
-    //initGrid<<<(N + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>>(grid);
     cudaDeviceSynchronize();
     cudaMemcpy(h_grid, grid, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    updatePotential<<<gridSize, blockSize>>>(potential, grid);
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        printf("CUDA error in updatePotential: %s\n", cudaGetErrorString(err));
+    }
+    cudaDeviceSynchronize();
+    cudaMemcpy(h_potential, potential, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+
 
     //print the grid:
     for (int i = 0; i < N; ++i) {
@@ -310,7 +334,7 @@ int main() {
     for (int i = 0; i < N_STEPS; ++i) {
         printf("Step %d\n", i);
 
-        moveAgents<<<(WORM_COUNT + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>>(d_agents, d_states, grid);
+        moveAgents<<<(WORM_COUNT + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE>>>(d_agents, d_states, grid, potential);
         // Check for errors in the kernel launch
         cudaError_t err = cudaGetLastError();
         if (err != cudaSuccess) {
@@ -320,7 +344,6 @@ int main() {
 
         // Copy data from device to host
         cudaMemcpy(h_agents, d_agents, size, cudaMemcpyDeviceToHost);
-        //cudaMemcpy(h_grid, grid, N * N * sizeof(float), cudaMemcpyDeviceToHost);
 
         //update grid
         updateGrid<<<gridSize, blockSize>>>(grid);
@@ -332,13 +355,24 @@ int main() {
         // copy data from device to host
         cudaMemcpy(h_grid, grid, N * N * sizeof(float), cudaMemcpyDeviceToHost);
 
+        //update potential
+        updatePotential<<<gridSize, blockSize>>>(potential, grid);
+        err = cudaGetLastError();
+        if (err != cudaSuccess) {
+            printf("CUDA error in updatePotential: %s\n", cudaGetErrorString(err));
+        }
+        cudaDeviceSynchronize();
+        cudaMemcpy(h_potential, potential, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+
         //check if any value in grid is invalid
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (isnan(h_grid[i * N + j]) || isinf(h_grid[i * N + j])) {
-                    printf("Invalid concentration %f at (%d, %d)\n", h_grid[i * N + j], i, j);
-                    broken = true;
-                    break;
+        if (DEBUG){
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < N; ++j) {
+                    if (isnan(h_grid[i * N + j]) || isinf(h_grid[i * N + j])) {
+                        printf("Invalid concentration %f at (%d, %d)\n", h_grid[i * N + j], i, j);
+                        broken = true;
+                        break;
+                    }
                 }
             }
         }
@@ -354,7 +388,10 @@ int main() {
 
     cudaFree(d_agents);
     cudaFree(d_states);
+    cudaFree(grid);
+    cudaFree(potential);
     delete[] h_agents;
     delete[] h_grid;
+    delete[] h_potential;
     return 0;
 }
